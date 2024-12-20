11bbe3af0d73edc2794152dd37d49c4df4c5fe94
Samuel Fessehaye <samuel.fessehaye123@gmail.com>
2024-12-20 07:32:59 -0500

    style update

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -92,7 +92,7 @@ function SearchPage() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
+    <div className=" flex h-screen w-full min-h-screen  flex-col items-center justify-center bg-gray-100 p-4">
       <h1 className="text-3xl font-bold mb-6">ምዕላደ ቃላት - dictionary</h1>
 
       {/* Language Selection Dropdown */}
