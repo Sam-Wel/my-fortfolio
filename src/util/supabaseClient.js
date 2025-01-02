32a75b9ce35e036d9e4e498b88f32d4fbfa10e9f
Samuel Fessehaye <samuel.fessehaye123@gmail.com>
2025-01-02 08:48:59 -0500

    enhanced search page

--- src/util/supabaseClient.js
+++ src/util/supabaseClient.js
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_SECRET_KEY = process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY; // Use the secret key for server-side access
+// Your Supabase project details
+const SUPABASE_URL = 'https://bkoyugjfthrenlhtfrft.supabase.co'; // Replace with your URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrb3l1Z2pmdGhyZW5saHRmcmZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzOTU0MjMsImV4cCI6MjA0Njk3MTQyM30.lAY4v9PnJK13g_OBRL4D6kpZA0mk4BpyzwOwoTAIOuM'; // Replace with your anon key
 
-if (!SUPABASE_URL || !SUPABASE_SECRET_KEY) {
-  throw new Error("Missing Supabase environment variables.");
-}
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_SECRET_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
